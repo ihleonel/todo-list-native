c876aff321614da8611ab20811814afd549d47a6
Ibarra Hector Leonel <ibarra.h.leonel@gmail.com>
2024-08-24 19:27:07 -0300

    Improve styles scroll

--- pages/Main.jsx
+++ pages/Main.jsx
@@ -30,9 +30,11 @@ export default function Main() {
       <Appbar.Header>
         <Appbar.Content title="Tasks" />
       </Appbar.Header>
-      <ScrollView contentContainerStyle={{ flex: 1, flexDirection: 'column', justifyContent: 'flex-end', paddingHorizontal: 10, paddingVertical: 10}}>
+      <ScrollView contentContainerStyle={{padding: 16}}>
           {tasks.map((task, index) => (
-            <Task key={index} task={task} remove={onRemove} />
+            <View key={index}>
+              <Task task={task} remove={onRemove} />
+            </View>
           ))}
       </ScrollView>
       <View style={{marginHorizontal: 10}}>
